46d75f0e24f02aea3bbad0bb1d554a902976e4b4
SzakacsAdam <szakacs.adam.98@gmail.com>
2024-09-20 17:14:42 +0200

    ADD: common apps that I could think of

--- install_scripts/install_commons.sh
+++ install_scripts/install_commons.sh
@@ -2,5 +2,80 @@
 
 set -eux
 
-apt-get install --yes --no-install-recommends \
-    net-tools
+install_core_apps() {
+    apt-get install --yes --no-install-recommends \
+        coreutils \
+        build-essential \
+        software-properties-common \
+        ca-certificates \
+        apt-transport-https
+}
+
+install_common_tools() {
+    apt-get install --yes --no-install-recommends \
+        curl \
+        wget \
+        jq \
+        openssh-client \
+        gnupg \
+        make
+}
+
+install_text_related() {
+    apt-get install --yes --no-install-recommends \
+        nano \
+        sed \
+        gawk \
+        grep
+}
+
+install_network_tools() {
+    apt-get install --yes --no-install-recommends \
+        net-tools \
+        nmap \
+        telnet \
+        dnsutils \
+        netcat-traditional \
+        netcat-openbsd \
+        traceroute \
+        tcpdump \
+        iperf3
+}
+
+install_file_related() {
+    apt-get install --yes --no-install-recommends \
+        mc \
+        tree \
+        rsync \
+        unzip \
+        zip
+}
+
+install_system_monitoring_tools() {
+    apt-get install --yes --no-install-recommends \
+        htop \
+        iotop \
+        iftop \
+        sysstat \
+        glances \
+        nmon
+}
+install_git () {
+    apt-get install --yes --no-install-recommends \
+        git
+    git config --global --add safe.directory '*'
+}
+
+main() {
+    apt-get update
+
+    install_core_apps
+    install_common_tools
+    install_text_related
+    install_network_tools
+    install_file_related
+    install_system_monitoring_tools
+    install_git
+}
+
+main
