1e80fabae6c92ee02ebd3fd97d462852b3dc6328
SzakacsAdam <szakacs.adam.98@gmail.com>
2024-09-22 14:07:41 +0200

    FIX: typo in python installer

--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -72,6 +72,7 @@ install_pyenv() {
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> "${HOME}/.bashrc"
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> "${HOME}/.bashrc"
     echo 'eval "$(pyenv init -)"' >> "${HOME}/.bashrc"
+    echo 'eval "$(pyenv virtualenv-init -)"' >> "${HOME}/.bashrc"
     source "${HOME}/.bashrc"
 }
 
